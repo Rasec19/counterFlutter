00bbccc597b777f3a61ebbbcea9af50434780e50
Rasec Camacho <rasec.camacho19@gmail.com>
2023-02-11 18:52:40 -0700

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.blue,
       body: Center(
         child: Text('Home Screen'),
       ),
